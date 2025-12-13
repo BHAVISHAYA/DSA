fc1be05ed87dbc37813738ac43a45657524d2122
BHAVISHAYA <bhavishayakhandelwal@gmail.com>
2025-12-13 09:22:48 +0530

    Arrays - 2025-12-13 - 6

---
+++ Arrays/16_Longest_SubArray_With_Given_Sum.cpp
@@ -0,0 +1,47 @@
+//todo - Que: 16 Longest sub-array with given sum 
+//todo - Link (https://www.naukri.com/code360/problems/longest-subarray-with-sum-k_5713505)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int lenOfLongestSubArrWithSumK(int *arr, int n, int k) {
+  map<long long int, int> m;
+  long long int sum = 0, length = 0;
+  for(int i = 0 ; i < n ; i++) {
+    sum += arr[i];
+    if(m.find(sum) == m.end()) {
+      m[sum] = i;
+    }
+    if(sum == k) {
+      if(length < i + 1) {
+        length = i + 1;
+      }
+    }
+    if(m.find(sum - k) != m.end()) {
+      int tempLen = i - m[sum - k];
+      if(length < tempLen) {
+        length = tempLen;
+      }
+    }
+  }
+  return length;
+}
+//* Time Complexity = O(n * log n)
+//* Space Complexity = O(n)
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  int arr[n];
+  for(int i = 0 ; i < n ; i++) {
+    cin >> arr[i];
+  }
+  cout << "Length of longest sub-array with sum k = " << lenOfLongestSubArrWithSumK(arr, n, k) << endl;
+}
+
+//* Input 
+// 4 3
+// 2 0 0 3
+
+//* Output 
+// Length = 3
